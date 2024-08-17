fa9f85d3f9141660f9a79786eed7dd875bb20d29
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-18 00:40:34 +0530

    generated file: `utils/constants.js`

---
+++ utils/constants.js
@@ -0,0 +1,26 @@
+export const API_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+export const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const GOAL_TYPES = [
+  "Weight Loss",
+  "Muscle Gain",
+  "Endurance Training",
+  "Flexibility Improvement",
+  "Calorie Tracking",
+  "Other",
+];
+
+export const SOCIAL_MEDIA_LINKS = {
+  facebook: "https://www.facebook.com/sharer/sharer.php?u=",
+  twitter: "https://twitter.com/intent/tweet?text=&url=",
+  linkedin: "https://www.linkedin.com/sharing/share-offsite/?url=",
+};
+
+export const CHART_COLORS = [
+  "hsl(20, 70%, 50%)",
+  "hsl(50, 70%, 50%)",
+  "hsl(80, 70%, 50%)",
+  "hsl(110, 70%, 50%)",
+  "hsl(140, 70%, 50%)",
+  "hsl(170, 70%, 50%)",
+];
